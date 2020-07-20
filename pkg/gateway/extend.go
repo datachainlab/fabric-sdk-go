25f9eccbd65191d86bb7ffb2d36fbf9d905fc443
toshihiko-okubo <toshihiko.okubo@speee.jp>
2020-07-21 01:33:19 +0900

    revert to query tx

--- pkg/gateway/extend.go
+++ pkg/gateway/extend.go
@@ -8,10 +8,11 @@ import (
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
 )
 
-// Execute a transaction to the ledger. The transaction function represented by this object
-// will be evaluated on the endorsing peers and then submitted to the ordering service
-// for committing to the ledger.
-func (txn *Transaction) Execute(args ...string) (*channel.Response, error) {
+// Query a transaction function and return its results.
+// The transaction function will be evaluated on the endorsing peers but
+// the responses will not be sent to the ordering service and hence will
+// not be committed to the ledger. This can be used for querying the world state.
+func (txn *Transaction) Query(args ...string) (*channel.Response, error) {
 	bytes := make([][]byte, len(args))
 	for i, v := range args {
 		bytes[i] = []byte(v)
@@ -19,18 +20,14 @@ func (txn *Transaction) Execute(args ...string) (*channel.Response, error) {
 	txn.request.Args = bytes
 
 	var options []channel.RequestOption
-	if txn.endorsingPeers != nil {
-		options = append(options, channel.WithTargetEndpoints(txn.endorsingPeers...))
-	}
-	options = append(options, channel.WithTimeout(fab.Execute, txn.contract.network.gateway.options.Timeout))
+	options = append(options, channel.WithTimeout(fab.Query, txn.contract.network.gateway.options.Timeout))
 
-	response, err := txn.contract.client.InvokeHandler(
-		newSubmitHandler(txn.eventch),
+	response, err := txn.contract.client.Query(
 		*txn.request,
 		options...,
 	)
 	if err != nil {
-		return nil, errors.Wrap(err, "Failed to submit")
+		return nil, errors.Wrap(err, "Failed to evaluate")
 	}
 
 	return &response, nil
