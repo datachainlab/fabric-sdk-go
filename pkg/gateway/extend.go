fbf2ef669ff3c38c075a7d19352b6454890524e2
Ryo Sato <ryo.sato@speee.jp>
2020-12-09 13:09:56 +0900

    enhance Gateway to get Network with event client opts

--- pkg/gateway/extend.go
+++ pkg/gateway/extend.go
@@ -1,3 +1,9 @@
+/*
+Copyright 2020 Datachain All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package gateway
 
 import (
@@ -5,7 +11,10 @@ import (
 
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel/invoke"
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/event"
+	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/context"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
+	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
 )
 
 // Execute a transaction to the ledger. The transaction function represented by this object
@@ -67,4 +76,50 @@ func (txn *Transaction) Simulate(args ...string) (*channel.Response, error) {
 	}
 
 	return &response, nil
+}
+
+// GetNetworkWithEventOption returns an object representing a network channel.
+//  Parameters:
+//  name is the name of the network channel
+//  eventOpts is the event client options for the event service of the network
+//
+//  Returns:
+//  A Network object representing the channel
+func (gw *Gateway) GetNetworkWithEventOption(name string, eventOpts ...event.ClientOption) (*Network, error) {
+	var channelProvider context.ChannelProvider
+	if gw.options.Identity != nil {
+		channelProvider = gw.sdk.ChannelContext(name, fabsdk.WithIdentity(gw.options.Identity), fabsdk.WithOrg(gw.org))
+	} else {
+		channelProvider = gw.sdk.ChannelContext(name, fabsdk.WithUser(gw.options.User), fabsdk.WithOrg(gw.org))
+	}
+	return newNetworkWithEventOption(gw, channelProvider, eventOpts...)
+}
+
+// newNetworkWithEventOption returns an object representing a network channel with the explicit event client options.
+func newNetworkWithEventOption(gateway *Gateway, channelProvider context.ChannelProvider, eventOpts ...event.ClientOption) (*Network, error) {
+	n := Network{
+		gateway: gateway,
+	}
+
+	// Channel client is used to query and execute transactions
+	client, err := channel.New(channelProvider)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create new channel client")
+	}
+
+	n.client = client
+
+	ctx, err := channelProvider()
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create new channel context")
+	}
+
+	n.name = ctx.ChannelID()
+
+	n.event, err = event.New(channelProvider, eventOpts...)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create new event client")
+	}
+
+	return &n, nil
 }
